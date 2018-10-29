21c043ee324e7abf71f591e6db49f9014aef4de2
unknown <anioua@gmail.com>
2018-10-29 23:35:24 +0100

    work prog v2

--- js/main.js
+++ js/main.js
@@ -39,64 +39,95 @@ $(document).ready(function () {
             $('#AUprice').html(data[0].cena);
             //Wyliczona w NBP cena 1 g złota (w próbie 1000)
 
-            //            console.log(data[0].data);
 
         })
+        
+        
+    function currencyFixTwo(currency) {
+        var fixedCurrency = currency.toFixed(2);
+        return fixedCurrency;
+    }
 
         $.getJSON('http://api.nbp.pl/api/exchangerates/rates/c/usd/', function (data) {
             $('#USDcode').html(data.code);
-            $('#USDpriceAsk').html(data.rates[0].ask);
-            $('#USDpriceBid').html(data.rates[0].bid);
+            
+            var usdFixedAsk = currencyFixTwo(data.rates[0].ask);
+            var usdFixedBid = currencyFixTwo(data.rates[0].bid);
+                     
+            $('#USDpriceAsk').html(usdFixedAsk);
+            $('#USDpriceBid').html(usdFixedBid);
         })
         $.getJSON('http://api.nbp.pl/api/exchangerates/rates/c/eur/', function (data) {
             $('#EURcode').html(data.code);
-            $('#EURpriceAsk').html(data.rates[0].ask);
-            $('#EURpriceBid').html(data.rates[0].bid);
+            
+            var eurFixedAsk = currencyFixTwo(data.rates[0].ask);
+            var eurFixedBid = currencyFixTwo(data.rates[0].bid);
+            
+            $('#EURpriceAsk').html(eurFixedAsk);
+            $('#EURpriceBid').html(eurFixedBid);
 
         })
         $.getJSON('http://api.nbp.pl/api/exchangerates/rates/c/gbp/', function (data) {
             $('#GBPcode').html(data.code);
-            $('#GBPpriceAsk').html(data.rates[0].ask);
-            $('#GBPpriceBid').html(data.rates[0].bid);
+            
+            var gbpFixedAsk = currencyFixTwo(data.rates[0].ask);
+            var gbpFixedBid = currencyFixTwo(data.rates[0].bid);
+            
+            $('#GBPpriceAsk').html(gbpFixedAsk);
+            $('#GBPpriceBid').html(gbpFixedBid);
 
         })
         $.getJSON('http://api.nbp.pl/api/exchangerates/rates/c/chf/', function (data) {
             $('#CHFcode').html(data.code);
-            $('#CHFpriceAsk').html(data.rates[0].ask);
-            $('#CHFpriceBid').html(data.rates[0].bid);
+            
+            var chfFixedAsk = currencyFixTwo(data.rates[0].ask);
+            var chfFixedBid = currencyFixTwo(data.rates[0].bid);
+            
+            $('#CHFpriceAsk').html(chfFixedAsk);
+            $('#CHFpriceBid').html(chfFixedBid);
 
         })
         $.getJSON('http://api.nbp.pl/api/exchangerates/rates/c/jpy/', function (data) {
             $('#JPYcode').html(data.code);
-            $('#JPYpriceAsk').html(data.rates[0].ask);
-            $('#JPYpriceBid').html(data.rates[0].bid);
+            
+            var jpyFixedAsk = currencyFixTwo(data.rates[0].ask);
+            var jpyFixedBid = currencyFixTwo(data.rates[0].bid);
+            
+            $('#JPYpriceAsk').html(jpyFixedAsk);
+            $('#JPYpriceBid').html(jpyFixedBid);
 
         })
         $.getJSON('http://api.nbp.pl/api/exchangerates/rates/c/huf/', function (data) {
             $('#HUFcode').html(data.code);
-            $('#HUFpriceAsk').html(data.rates[0].ask);
-            $('#HUFpriceBid').html(data.rates[0].bid);
+            
+            var hufFixedAsk = currencyFixTwo(data.rates[0].ask);
+            var hufFixedBid = currencyFixTwo(data.rates[0].bid);
+            
+            $('#HUFpriceAsk').html(hufFixedAsk);
+            $('#HUFpriceBid').html(hufFixedBid);
 
         })
         $.getJSON('http://api.nbp.pl/api/exchangerates/rates/c/czk/', function (data) {
             $('#CZKcode').html(data.code);
-            $('#CZKpriceAsk').html(data.rates[0].ask);
-            $('#CZKpriceBid').html(data.rates[0].bid);
+            
+            var czkFixedAsk = currencyFixTwo(data.rates[0].ask);
+            var czkFixedBid = currencyFixTwo(data.rates[0].bid);
+            
+            $('#CZKpriceAsk').html(czkFixedAsk);
+            $('#CZKpriceBid').html(czkFixedBid);
 
         })
         $.getJSON('http://api.nbp.pl/api/exchangerates/rates/c/nok/', function (data) {
             $('#NOKcode').html(data.code);
-            $('#NOKpriceAsk').html(data.rates[0].ask);
-            $('#NOKpriceBid').html(data.rates[0].bid);
+            
+            var nokFixedAsk = currencyFixTwo(data.rates[0].ask);
+            var nokFixedBid = currencyFixTwo(data.rates[0].bid);
+            
+            $('#NOKpriceAsk').html(nokFixedAsk);
+            $('#NOKpriceBid').html(nokFixedBid);
 
         })
-        $.getJSON('http://api.nbp.pl/api/exchangerates/rates/c/hrk/', function (data) {
-            $('#HRKcode').html(data.code);
-            $('#HRKpriceAsk').html(data.rates[0].ask);
-            $('#HRKpriceBid').html(data.rates[0].bid);
-
-        })
-
+        
     }
 
 
